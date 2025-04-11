184d0903eba3e40e03e05b573822b706186543cb
anielmelo <anielcontato@gmail.com>
2025-04-11 14:01:49 -0300

    chore: add validation to dto

--- back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/dto/PostRequestDTO.java
+++ back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/dto/PostRequestDTO.java
@@ -1,4 +1,16 @@
 package br.edu.ifpb.esperanca.ads.diarymanager.diary.dto;
 
-public record PostRequestDTO(String title, String text, String image) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PostRequestDTO(
+    @NotBlank(message = "title cannot be blank.")
+    @NotNull(message = "title is required.")
+    String title, 
+    @NotBlank(message = "text cannot be blank.")
+    @NotNull(message = "text is required.")
+    String text, 
+    @NotBlank(message = "image cannot be blank.")
+    @NotNull(message = "image is required.")
+    String image) {
 }
