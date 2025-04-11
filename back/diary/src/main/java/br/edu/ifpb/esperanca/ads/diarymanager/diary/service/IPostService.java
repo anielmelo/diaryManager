b24fd693c5213ac305b202a83356e4d289522b6a
anielmelo <anielcontato@gmail.com>
2025-04-11 19:59:27 -0300

    chore: update injection dependency

---
+++ back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/service/IPostService.java
@@ -0,0 +1,7 @@
+package br.edu.ifpb.esperanca.ads.diarymanager.diary.service;
+
+import br.edu.ifpb.esperanca.ads.diarymanager.diary.dto.PostRequestDTO;
+import br.edu.ifpb.esperanca.ads.diarymanager.diary.dto.PostResponseDTO;
+
+public interface IPostService extends IService<PostRequestDTO, PostResponseDTO, Long> {
+}
--- back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/service/PostService.java
+++ back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/service/PostService.java
@@ -1,48 +1,17 @@
 package br.edu.ifpb.esperanca.ads.diarymanager.diary.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import br.edu.ifpb.esperanca.ads.diarymanager.diary.domain.Post;
 import br.edu.ifpb.esperanca.ads.diarymanager.diary.dto.PostRequestDTO;
 import br.edu.ifpb.esperanca.ads.diarymanager.diary.dto.PostResponseDTO;
-import br.edu.ifpb.esperanca.ads.diarymanager.diary.service.exception.PostNotFoundException;
+import br.edu.ifpb.esperanca.ads.diarymanager.diary.mapper.PostMapper;
+import br.edu.ifpb.esperanca.ads.diarymanager.diary.repository.PostRepository;
 
 @Service
 public class PostService extends BaseService<PostRequestDTO, PostResponseDTO, Post, Long> {
 
-    @Override
-    public void create(PostRequestDTO requestDTO) {
-        super.create(requestDTO);
+    public PostService(PostRepository repository, PostMapper mapper) {
+        super(repository, mapper);
     }
-
-    @Override
-    public void update(Long id, PostRequestDTO requestDTO) {
-        Post postToUpdate = repository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
-        
-        String title = requestDTO.title();
-        String text = requestDTO.text();
-        String image = requestDTO.image();
-
-        postToUpdate = new Post(id, title, text, image, postToUpdate.getDate());
-        
-        repository.save(postToUpdate);
-    }
-
-    @Override
-    public void delete(Long id) {
-        super.delete(id);
-    }
-
-    @Override
-    public List<PostResponseDTO> findAll() {
-        return super.findAll();
-    }
-
-    @Override
-    public PostResponseDTO findById(Long id) {
-        return super.findById(id);
-    }
-
 }
