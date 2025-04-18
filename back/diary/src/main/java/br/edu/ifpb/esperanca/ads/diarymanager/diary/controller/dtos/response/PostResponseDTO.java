81699ac202ca29e85c0d7b20a662036ef9437204
dinizhugo <hugodiniztech01@gmail.com>
2025-04-17 21:50:08 -0300

    feat[dto]: add dto's

---
+++ back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/controller/dtos/response/PostResponseDTO.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.esperanca.ads.diarymanager.diary.controller.dtos.response;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record PostResponseDTO(
+        Long id,
+        String title,
+        String text,
+        String image,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
+        LocalDateTime createdAt
+) {}
