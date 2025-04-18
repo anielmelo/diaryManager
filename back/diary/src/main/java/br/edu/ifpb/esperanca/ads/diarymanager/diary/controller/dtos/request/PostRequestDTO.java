4fd0d1611ca1992d44b094fd48c4d22d3f526b0b
anielmelo <anielcontato@gmail.com>
2025-04-18 15:57:22 -0300

    fix: alter dto attributes

--- back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/controller/dtos/request/PostRequestDTO.java
+++ back/diary/src/main/java/br/edu/ifpb/esperanca/ads/diarymanager/diary/controller/dtos/request/PostRequestDTO.java
@@ -1,14 +1,8 @@
 package br.edu.ifpb.esperanca.ads.diarymanager.diary.controller.dtos.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDateTime;
-
 public record PostRequestDTO(
         String title,
         String text,
-        String image,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
-        LocalDateTime createdAt
+        String image
 ) {}
 
